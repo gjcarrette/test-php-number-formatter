dbafe79c39196d767ccfaaf4e6a1a67532d910d6
Pardeep Kumar <pardeep26k@gmail.com>
2022-08-16 14:08:45 +0200

    NEW test file to show space issue

---
+++ Test2.php
@@ -0,0 +1,29 @@
+<?php
+
+function test0() {
+ if (function_exists('posix_uname')) {
+  $u = posix_uname();
+  unset($u['nodename']);
+  unset($u['domainname']);
+  echo json_encode($u, JSON_PRETTY_PRINT) . "\n";
+ }
+ $x = new \ReflectionExtension('intl');
+ $x->info();
+ echo "\n";
+}
+
+function test1($l = 'fr_CA', $m = 92345.67) {
+ echo "Locale::setDefault($l) = " . var_export(\Locale::setDefault($l), true) . "\n";
+ echo "Locale::getDefault() = " . \Locale::getDefault() . "\n";
+ $fmt = \NumberFormatter::create($l, \NumberFormatter::CURRENCY);
+ echo "CURRENCY_SYMBOL = " . var_export($fmt->getSymbol(\NumberFormatter::CURRENCY_SYMBOL), true) . "\n";
+ echo "INT_CURRENCY_SYMBOL = " . var_export($fmt->getSymbol(\NumberFormatter::INTL_CURRENCY_SYMBOL), true) . "\n";
+ echo "CURRENCY_CODE = " . var_export($fmt->getTextAttribute(NumberFormatter::CURRENCY_CODE), true) . "\n";
+ $r = $fmt->formatCurrency($m,'USD');
+ //$r = $fmt->format($m);
+ $h = bin2hex($r);
+ echo "format($m) = " . var_export($r, true) . " [$h]\n\n";
+}
+
+test0();
+test1('fr_CA');
